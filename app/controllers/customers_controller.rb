3df21c1f7f2a09ad194c6cce27940cfdff5de78c
Kyle Geske <stungeye@gmail.com>
2018-03-21 19:51:34 +0000

    Simple bootstrap styling of customers.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,13 @@
 class CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def missing_email
+    @customers = Customer.where(email: '')
   end
 end
